f59669cf3539a15e7c2cc0b9dc894186c5b089f3
Matthew Nerger <mnerger@lsu.edu>
2016-04-01 17:16:17 -0500

    Add explanatory comments

--- js/script.js
+++ js/script.js
@@ -1,22 +1,23 @@
 
 function loadData() {
-
+    //define page element variables
     var $body = $('body');
     var $wikiElem = $('#wikipedia-links');
     var $nytHeaderElem = $('#nytimes-header');
     var $nytElem = $('#nytimes-articles');
     var $greeting = $('#greeting');
 
-
+    //take in user entered address
     var street = $('#street').val();
     var city = $('#city').val();
     var address = street + ', ' + city;
 
+    //change the greeting to match the entered text
     $greeting.text("So you're looking at " + address + "?");
 
+    //add streetview background image
     var streetviewURL = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + ' ';
     $body.append('<img class="bgimg" src="' + streetviewURL + '">');
-
 
     // clear out old data before new request
     $wikiElem.text("");

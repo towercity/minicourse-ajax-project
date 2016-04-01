645b37ddab9b6c5d6f937b1df2af4794098ba087
Matthew Nerger <mnerger@lsu.edu>
2016-04-01 17:13:42 -0500

    Add streetview background

--- js/script.js
+++ js/script.js
@@ -7,6 +7,17 @@ function loadData() {
     var $nytElem = $('#nytimes-articles');
     var $greeting = $('#greeting');
 
+
+    var street = $('#street').val();
+    var city = $('#city').val();
+    var address = street + ', ' + city;
+
+    $greeting.text("So you're looking at " + address + "?");
+
+    var streetviewURL = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + ' ';
+    $body.append('<img class="bgimg" src="' + streetviewURL + '">');
+
+
     // clear out old data before new request
     $wikiElem.text("");
     $nytElem.text("");
